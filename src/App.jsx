2897076148fa31d91982afcd003aa434313ae8cb
009 <bleh.ihh@gmail.com>
2026-03-18 10:57:30 +0200

    update exercitiul 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,38 @@
+import { useState } from 'react'; 
 import Card from './Card';
 
 function App() {
   
+  const [count, setCount] = useState(0);
+
   const projects = [
-    { title: "Proiect 1", description: "Pagina personala cu HTML si CSS" },
-    { title: "Proiect 2", description: "Calculator de buget în JS" },
-    { title: "Proiect 3", description: "Dashboard React Complex" },
-    { title: "Proiect 4", description: "Magazin online " },
+    { title: "Proiect 1", description: "Pagina personala" },
+    { title: "Proiect 2", description: "Calculator buget" },
+    { title: "Proiect 3", description: "Dashboard React" },
+    { title: "Proiect 4", description: "Magazin online" },
     { title: "Proiect 5", description: "Aplicatie de vreme" }
   ];
 
   return (
-    <div style={{ padding: '20px' }}>
-      <h1>Portofoliul Meu Dinamic</h1>
-      
-      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-        {projects.map(function(item, index) {
-          return (
-            <Card 
-              key={index} 
-              title={item.title} 
-              description={item.description} 
-            />
-          );
-        })}
+    <div style={{ padding: '20px', textAlign: 'center' }}>
+      <h1>Interactivitate în React</h1>
+
+  
+      <div style={{ margin: '20px', padding: '10px', background: '#eee' }}>
+        <p>Butonul a fost apăsat de <strong>{count}</strong> ori</p>
+        
+    
+        <button onClick={() => setCount(count + 1)}>
+          Apasă-mă!
+        </button>
+      </div>
+
+      <hr />
+
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        {projects.map((item, index) => (
+          <Card key={index} title={item.title} description={item.description} />
+        ))}
       </div>
     </div>
   );
