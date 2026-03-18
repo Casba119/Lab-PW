a9cc50692073b4e32ef968803330fa756b45cc5c
009 <bleh.ihh@gmail.com>
2026-03-18 10:50:55 +0200

    update exercitiul 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
-import Card from './Card';  
+import Card from './Card';
 
 function App() {
+  
+  const projects = [
+    { title: "Proiect 1", description: "Pagina personala cu HTML si CSS" },
+    { title: "Proiect 2", description: "Calculator de buget în JS" },
+    { title: "Proiect 3", description: "Dashboard React Complex" },
+    { title: "Proiect 4", description: "Magazin online " },
+    { title: "Proiect 5", description: "Aplicatie de vreme" }
+  ];
+
   return (
     <div style={{ padding: '20px' }}>
-      <h1>Dashboard</h1>
-      <Card
-        title="Proiect 1" 
-        description="Pagina personala cu HTML si CSS" " />
-      <Card
-      title="Proiect 2" 
-      description="Pagina interactiva cu Javascript "/>
-      <Card
-      title="Proiect 3"
-      description="Dashboard cu React" />
+      <h1>Portofoliul Meu Dinamic</h1>
+      
+      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+        {projects.map(function(item, index) {
+          return (
+            <Card 
+              key={index} 
+              title={item.title} 
+              description={item.description} 
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
