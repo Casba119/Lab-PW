9dd7dd3ba6e480990fd2edd08063b0625585192c
009 <bleh.ihh@gmail.com>
2026-05-06 11:21:33 +0300

    lab 9 ex 6

--- server/index.js
+++ server/index.js
@@ -59,9 +59,23 @@ app.get('/api/projects', async function(req, res) {
         const projects = await Project.find();
         res.json(projects);
     } catch (err) {
-        res.status(500).json({ error: 'Eroare ' + err });
+        res.status(500).json({ error: 'Eroare la preluare date' });
     }
 });
+
+//GET - returneaza un singur proiect dupa id
+app.get('/api/projects/:id', async function(req, res) {
+    try {
+        const project = await Project.findById(req.params.id);
+        if (!project) {
+            return res.status(404).json({ error: 'nema project' });
+        }
+        res.json(project);
+    } catch (err) {
+        res.status(400).json({ error: 'ID invalid' });
+    }
+});
+
 
 // POST /api/projects - adauga un proiect nou
 app.post('/api/projects', async function(req, res) {
@@ -80,16 +94,18 @@ app.post('/api/projects', async function(req, res) {
 });
 
 // DELETE /api/projects/:id 
-app.delete('/api/projects/:id', function(req, res) {
-     const id = parseInt(req.params.id);
-     const index = projects.findIndex(p => p.id === id);
+app.delete('/api/projects/:id', async function(req, res) {
+    try {
+        const deletedProject = await Project.findByIdAndDelete(req.params.id);
 
-     if (index === -1) {
-         return res.status(404).json({ error: 'Not found' });
-     }
+        if (!deletedProject) {
+            return res.status(404).json({ error: 'Proiectul nu există' });
+        }
 
-     projects.splice(index, 1);
-     res.json({ message: 'Sters' });
+        res.json({ message: 'Sters' });
+    } catch (err) {
+        res.status(500).json({ error: 'eroare la stergere ' + err.message });
+    }
 });
 
 // Porneste serverul
