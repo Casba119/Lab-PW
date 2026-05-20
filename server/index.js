c29b1f2a25535f462f05255fbc337a5429907c33
009 <bleh.ihh@gmail.com>
2026-05-21 02:21:07 +0300

    fix: actualizat CORS pentru link-ul de Vercel

--- server/index.js
+++ server/index.js
@@ -6,7 +6,11 @@ dns.setDefaultResultOrder('ipv4first');
 const express = require('express');
 const cors = require('cors');
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5173', 'https://lab-pw.vercel.app'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type']
+}));
 const PORT = process.env.PORT || 3000;
 const mongoose = require('mongoose');
 const Project = require('./models/Project');
